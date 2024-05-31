402e9a66171ebdd36394bddbbb23b4e20b2e64c3
Aleksei Nushtaev <nushtaev2110@gmail.com>
2024-05-31 16:31:13 +0500

    Fix text

--- handlers.py
+++ handlers.py
@@ -70,21 +70,22 @@ async def process_start(msg: Message, state: FSMContext):
     async with (Session() as session):
         query = select(User.user_id).where(User.user_id == str(msg.from_user.id))
         results = await session.execute(query)
-        if not results.scalar():
-            stmt = insert(User).values(
-                user_id=str(msg.from_user.id),
-                username=msg.from_user.username,
-                first_name=msg.from_user.first_name,
-                last_name=msg.from_user.last_name,
-                state="Активен",
-                time_start=datetime.now()
-            )
-            await session.execute(stmt)
-            await session.commit()
-            await msg.answer(text="Привет! Это компания Старт!\n\nПожалуйста, напишите Ваше ФИО.")
-            await state.set_state(FSMFillForm.fill_name)
-        else:
-            await msg.answer(text="Вы уже участвуете в розыгрыше. Ждите результатов - мы сообщим.")
+        #if not results.scalar():
+        stmt = insert(User).values(
+            user_id=str(msg.from_user.id),
+            username=msg.from_user.username,
+            first_name=msg.from_user.first_name,
+            last_name=msg.from_user.last_name,
+            state="Активен",
+            time_start=datetime.now()
+        )
+        await session.execute(stmt)
+        await session.commit()
+        await msg.answer(text="Привет! Это компания Старт!\n\nПожалуйста, напишите Ваше ФИО.\n\n"
+                              "Пример: Иванов Иван Иванович")
+        await state.set_state(FSMFillForm.fill_name)
+        # else:
+        #     await msg.answer(text="Вы уже участвуете в розыгрыше. Ждите результатов - мы сообщим.")
 
 
 @router.message(
@@ -96,35 +97,50 @@ async def process_name_sent(msg: Message, state: FSMContext):
                                                                                 time_name=datetime.now())
         await session.execute(stmt)
         await session.commit()
-    await msg.answer(text='Спасибо!\n\nТеперь, пожалуйста, укажите номер вашего договора.')
+    await msg.answer(text='Спасибо!\n\nТеперь, пожалуйста, укажите номер вашего договора.\n\n'
+                          'Пример номера договора: 31/05/24-2085-01Б, указан на бланке вверху.')
     await state.set_state(FSMFillForm.fill_contract)
 
 
 @router.message(StateFilter(FSMFillForm.fill_name))
 async def process_name_wrong(msg: Message):
-    await msg.answer(text='Что-то пошло не так\n'
-                              'ФИО должно состоять из букв\n'
+    await msg.answer(text='Что-то пошло не так\n\n'
+                              'ФИО должно состоять из букв\n\n'
+                              'Пример: Иванов Иван Иванович\n\n'
                               'Пожалуйста введите повторно ФИО')
 
 
-@router.message(
-    StateFilter(FSMFillForm.fill_contract),
-    lambda m: m.text.replace(' ', '').isdigit() is True)
+@router.message(StateFilter(FSMFillForm.fill_contract), F.text)
 async def process_contract_sent(msg: Message, state: FSMContext):
     async with (Session() as session):
         stmt = update(User).where(User.user_id == str(msg.from_user.id)).values(contract=msg.text,
                                                                                 time_contract=datetime.now())
         await session.execute(stmt)
         await session.commit()
-    await msg.answer(text='Спасибо!\n\nТеперь, пожалуйста, укажите ваш ник на YouTube.')
+    await msg.answer(
+        text='Спасибо!\n\nТеперь, пожалуйста, укажите ваш ник на YouTube.\n\n'
+             'Справка для поиска ника в YouTube:\n\n'
+             '1. Откройте YouTube:\n'
+             '- Перейдите на сайт [YouTube](https://www.youtube.com) или откройте приложение YouTube на вашем устройстве.\n\n'
+             '2. Войдите в свой аккаунт:\n'
+             '- Если вы еще не вошли в свой аккаунт, нажмите на кнопку "Войти" в правом верхнем углу экрана и введите свои учетные данные.\n\n'
+             '3. Перейдите в раздел "Вы":\n'
+             '- Нажмите на иконку вашего профиля в правом верхнем углу экрана. Это может быть ваша фотография или первая буква вашего имени.\n\n'
+             '4. Откройте ваш канал:\n'
+             '- В выпадающем меню выберите "Ваш канал". Это перенаправит вас на страницу вашего канала.\n\n'
+             '5. Найдите ваш ник:\n'
+             '- На странице вашего канала, под вашим именем, вы увидите ваш ник (псевдоним). Он будет отображаться как часть URL вашего канала или рядом с вашим именем.\n\n'
+             'Пример ника: user-gj8yn1hq9v\n\n'
+             'Если у вас возникли трудности или дополнительные вопросы, пожалуйста, дайте знать!'
+    )
     await state.set_state(FSMFillForm.fill_tube)
 
 
 @router.message(StateFilter(FSMFillForm.fill_contract))
 async def process_contract_wrong(msg: Message):
-    await msg.answer(text='Что-то пошло не так\n'
-                              'Номер договора должен состоять из цифр\n'
-                              'Пожалуйста введите повторно номер договора')
+    await msg.answer(text='Что-то пошло не так\n\n'
+                          'Пример номера договора: 31/05/24-2085-01Б, указан на бланке вверху.\n\n'
+                          'Пожалуйста введите повторно номер договора')
 
 
 @router.message(StateFilter(FSMFillForm.fill_tube), F.text)
@@ -134,14 +150,38 @@ async def process_youtube_sent(msg: Message, state: FSMContext):
                                                                                 time_tube=datetime.now())
         await session.execute(stmt)
         await session.commit()
-    await msg.answer(text='Спасибо!\n\nТеперь, пожалуйста, укажите ваш ник в ВКонтакте.')
+    await msg.answer(text='Спасибо!\n\n'
+                          'Теперь, пожалуйста, укажите ссылку на Вашу страницу в ВКонтакте.\n\n'
+                          'Справка для поиска ссылки страницы в Вконтакте:\n\n'
+                          '1. Откройте браузер и перейдите на сайт ВКонтакте (vk.com) или откройте приложение.\n'
+                          '2. Войдите в свой аккаунт, если вы еще этого не сделали.\n'
+                          '3. Перейдите на свою страницу профиля. Для этого нажмите на свое имя или аватар в '
+                          'верхнем правом углу экрана.\n'
+                          '4. В адресной строке браузера вы увидите URL-адрес вашей страницы.\n\n'
+                          'Он будет выглядеть примерно так: `https://vk.com/id123456789` или `https://vk.com/yourusername`.\n'
+                          '5. Скопируйте ссылку и отправьте её мне')
     await state.set_state(FSMFillForm.fill_vk)
 
 
 @router.message(StateFilter(FSMFillForm.fill_tube))
 async def process_youtube_wrong(msg: Message):
-    await msg.answer(text='Что-то пошло не так\n'
-                          'Напишите ник в текстовом сообщении')
+    await msg.answer(
+        text='Что-то пошло не так\n\n'
+             'Напишите ник YouTube в текстовом сообщении\n\n'
+             'Справка для поиска ника в Youtube\n\n'
+             '1. Откройте YouTube:\n'
+             '- Перейдите на сайт [YouTube](https://www.youtube.com) или откройте приложение YouTube на вашем устройстве.\n\n'
+             '2. Войдите в свой аккаунт:\n'
+             '- Если вы еще не вошли в свой аккаунт, нажмите на кнопку "Войти" в правом верхнем углу экрана и введите свои учетные данные.\n\n'
+             '3. Перейдите в раздел "Вы":\n'
+             '- Нажмите на иконку вашего профиля в правом верхнем углу экрана. Это может быть ваша фотография или первая буква вашего имени.\n\n'
+             '4. Откройте ваш канал:\n'
+             '- В выпадающем меню выберите "Ваш канал". Это перенаправит вас на страницу вашего канала.\n\n'
+             '5. Найдите ваш ник:\n'
+             '- На странице вашего канала, под вашим именем, вы увидите ваш ник (псевдоним). Он будет отображаться как часть URL вашего канала или рядом с вашим именем.\n\n'
+             'Пример ника: user-gj8yn1hq9v\n\n'
+             'Если у вас возникли трудности или дополнительные вопросы, пожалуйста, дайте знать!'
+    )
 
 
 @router.message(StateFilter(FSMFillForm.fill_vk), F.text)
@@ -151,14 +191,36 @@ async def process_vk_sent(msg: Message, state: FSMContext):
                                                                                 time_vk=datetime.now())
         await session.execute(stmt)
         await session.commit()
-    await msg.answer(text='Отлично!\n\n Пожалуйста, загрузите видео.')
+    await msg.answer(text='Отлично!\n\nПожалуйста, загрузите видео.\n\n'
+                          'Требования к видео:\n\n'
+                          '- Рассказ о вашей ситуации.\n\n'
+                          'Видеоролик не должен содержать:\n\n'
+                          '- Ненормативную лексику (мат, оскорбления)\n'
+                          '- Непристойное поведение\n'
+                          '- Распитие спиртных напитков\n'
+                          '- Курение\n'
+                          '- Употребление наркотических веществ\n'
+                          '- Людей в состоянии алкогольного/наркотического опьянения\n'
+                          '- Политические лозунги\n'
+                          '- Призывы к терроризму/экстремизму\n'
+                          '- Порнографические материалы'
+                     )
     await state.set_state(FSMFillForm.fill_video)
 
 
 @router.message(StateFilter(FSMFillForm.fill_vk))
 async def process_vk_wrong(msg: Message):
-    await msg.answer(text='Что-то пошло не так\n'
-                          'Напишите ник в текстовом сообщении')
+    await msg.answer(text='Что-то пошло не так\n\n'
+                          'Пришлите мне ссылку в текстовом сообщении\n\n'
+                          'Справка для поиска ссылки страницы в Вконтакте:\n\n'
+                          '1. Откройте браузер и перейдите на сайт ВКонтакте (vk.com) или откройте приложение.\n'
+                          '2. Войдите в свой аккаунт, если вы еще этого не сделали.\n'
+                          '3. Перейдите на свою страницу профиля. Для этого нажмите на свое имя или аватар в '
+                          'верхнем правом углу экрана.\n'
+                          '4. В адресной строке браузера вы увидите URL-адрес вашей страницы.\n\n'
+                          'Он будет выглядеть примерно так: `https://vk.com/id123456789` или `https://vk.com/yourusername`.\n'
+                          '5. Скопируйте ссылку и отправьте её мне'
+                     )
 
 
 @router.message(StateFilter(FSMFillForm.fill_video), F.video)
@@ -170,15 +232,29 @@ async def process_video_sent(msg: Message, state: FSMContext):
         await session.commit()
         query = select(User.id).where(User.user_id == str(msg.from_user.id))
         res = await session.execute(query)
-    await msg.answer(text=f'Спасибо за предоставленные ответы!\nВаш уникальный номер - {res.first()[0]}!\n'
-                          f'Желаю удачи в розыгрыше!')
+    await msg.answer(text=f'Спасибо за предоставленные ответы!\n\nВаш уникальный номер - {res.first()[0]}!\n'
+                          f'Желаю удачи в розыгрыше!\n\n'
+                          f'Если у вас возникли трудности или дополнительные вопросы, пожалуйста, дайте знать!')
     await state.set_state(default_state)
 
 
 @router.message(StateFilter(FSMFillForm.fill_video))
 async def process_video_file(msg: Message):
-    await msg.answer(text='Что-то пошло не так\n'
-                              'Пришлите пжл сообщение в формате видео')
+    await msg.answer(text='Что-то пошло не так\n\n'
+                          'Пришлите пжл сообщение в формате видео\n\n'
+                          'Требования к видео:\n\n'
+                          '- Рассказ о вашей ситуации.\n\n'
+                          'Видеоролик не должен содержать:\n\n'
+                          '- Ненормативную лексику (мат, оскорбления)\n'
+                          '- Непристойное поведение\n'
+                          '- Распитие спиртных напитков\n'
+                          '- Курение\n'
+                          '- Употребление наркотических веществ\n'
+                          '- Людей в состоянии алкогольного/наркотического опьянения\n'
+                          '- Политические лозунги\n'
+                          '- Призывы к терроризму/экстремизму\n'
+                          '- Порнографические материалы'
+                     )
 
 
 @router.message(CommandStart(), F.from_user.id.in_(ADMIN_IDS))
@@ -236,6 +312,9 @@ async def user_unblocked_bot(event: ChatMemberUpdated):
 @router.message(~F.from_user.id.in_(ADMIN_IDS), StateFilter(default_state))
 async def process_forward_msg(mes: Message):
     for chat_id in ADMIN_IDS:
-        await bot.forward_message(chat_id=chat_id, from_chat_id=mes.chat.id, message_id=mes.message_id)
+        try:
+            await bot.forward_message(chat_id=chat_id, from_chat_id=mes.chat.id, message_id=mes.message_id)
+        except Exception:
+            continue
 
 
